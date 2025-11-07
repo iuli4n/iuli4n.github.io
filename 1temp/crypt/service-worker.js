2c9c2b1a4896eb4557870c3e6736c4d4ce1536ab
Iulian Radu <infinite.flow@gmail.com>
2025-11-06 23:42:14 -0500

    .

---
+++ 1temp/crypt/service-worker.js
@@ -0,0 +1,28 @@
+// service-worker.js
+const CACHE_NAME = "encrypt-app-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./crypt.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"
+];
+
+// Install & cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Serve files from cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
